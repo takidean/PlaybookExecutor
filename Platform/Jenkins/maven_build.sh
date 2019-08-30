d58f7f5bb750067289315289c069e69c60f99ceb
takidean <takieddine.sdiri@gmail.com>
2019-08-30 15:27:12 +0200

    ADD platform

---
+++ Platform/Jenkins/maven_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+output_log=/tmp/nightly-setup
+
+function trap_on_failure() {
+	        echo "Error while setting the new versions. Printing the logs"
+		        cat $output_log
+		}
+
+		trap "trap_on_failure" ERR
+
+		version_date=$(date +%Y%m%d)
+
+   for projectOption in '' '-f sandboxes'
+   do
+	     mvn $projectOption build-helper:parse-version versions:set -DnewVersion='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}-'$version_date &> $output_log
+     done
+    
+		mvn versions:update-parent &> $output_log
+
+		mvn $projectOption build-helper:parse-version versions:set -DnewVersion='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}-'$version_date &>> $output_log
+
+		mvn versions:use-latest-versions -DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=false &>> $output_log
+
+		mvn versions:update-properties -N -DfailIfNotReplaced=true -DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=false &>> $output_log
+
+		mvn versions:commit &>> $output_log
+
